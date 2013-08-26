299bc667864ffe8711d36770b525a07db60618a2
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-08-26 16:01:38 -0400

    Initial CMake changes for module

--- itk-module.cmake
+++ itk-module.cmake
@@ -3,18 +3,20 @@
 get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 
-# itk_module() defines the module dependencies in ITKExternalTemplate
-# ITKExternalTemplate depends on ITKCommon
-# The testing module in ITKExternalTemplate depends on ITKTestKernel
-# and ITKMetaIO(besides ITKExternalTemplate and ITKCore)
+# itk_module() defines the module dependencies in ITKStreamingSinc
+# ITKStreamingSinc depends on ITKCommon
+# The testing module in ITKStreamingSinc depends on ITKTestKernel
+# and ITKMetaIO(besides ITKStreamingSinc and ITKCore)
 
 # define the dependencies of the include module and the tests
-itk_module(ITKExternalTemplate
+itk_module(StreamingSinc
   DEPENDS
     ITKCommon
+    ITKImageFilterBase
+    ITKStatistics
   TEST_DEPENDS
     ITKTestKernel
-    ITKMetaIO
+    ITKStatistics
   DESCRIPTION
     "${DOCUMENTATION}"
   EXCLUDE_FROM_ALL
