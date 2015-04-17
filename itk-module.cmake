35de19d08be0efedf90a28d8077a9e89ccfd1e6d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-04-17 07:49:31 -0400

    Update for newer exclude variable

--- itk-module.cmake
+++ itk-module.cmake
@@ -8,6 +8,12 @@ file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 # The testing module in ITKStreamingSinc depends on ITKTestKernel
 # and ITKMetaIO(besides ITKStreamingSinc and ITKCore)
 
+# ITK version 4.5 changed it from EXCLUDE_FROM_ALL to EXCLUDE_FROM_DEFAULT
+set( _EXCLUDE "EXCLUDE_FROM_ALL" )
+if (NOT "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_MINOR_PATCH}" VERSION_LESS "4.5")
+  set( _EXCLUDE "EXCLUDE_FROM_DEFAULT" )
+endif()
+
 # define the dependencies of the include module and the tests
 itk_module(StreamingSinc
   DEPENDS
@@ -20,5 +26,5 @@ itk_module(StreamingSinc
     ITKStatistics
   DESCRIPTION
     "${DOCUMENTATION}"
-  EXCLUDE_FROM_ALL
+  ${_EXCLUDE}
 )
