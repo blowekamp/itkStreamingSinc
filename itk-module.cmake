7faa037a634db2f650850af7bdd7ad9012eeea18
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-05-01 13:48:13 -0400

    Update exclude from all and module dependencies

--- itk-module.cmake
+++ itk-module.cmake
@@ -3,6 +3,13 @@
 get_filename_component( MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 file( READ "${MY_CURENT_DIR}/README" DOCUMENTATION )
 
+# ITK version 4.5 changed it from EXCLUDE_FROM_ALL to EXCLUDE_FROM_DEFAULT
+set( _EXCLUDE "EXCLUDE_FROM_ALL" )
+if (NOT "${ITK_VERSION_MAJOR}.${ITK_VERSION_MINOR}.${ITK_VERSION_MINOR_PATCH}" VERSION_LESS "4.5")
+  set( _EXCLUDE "EXCLUDE_FROM_DEFAULT" )
+endif()
+
+
 # itk_module() defines the module dependencies in ITKStreamingSinc
 # ITKStreamingSinc depends on ITKCommon
 # The testing module in ITKStreamingSinc depends on ITKTestKernel
@@ -21,9 +28,11 @@ itk_module(StreamingSinc
     ITKImageFilterBase
     ITKStatistics
     ITKImageStatistics
+    ITKImageGrid
   TEST_DEPENDS
     ITKTestKernel
     ITKStatistics
+    ITKSmoothing
   DESCRIPTION
     "${DOCUMENTATION}"
   ${_EXCLUDE}
